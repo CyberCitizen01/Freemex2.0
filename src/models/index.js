59e4d7217fbbeb0624648cbde8da8efaaa5d55f7
CyberCitizen01 <mr.20u10448@btech.nitdgp.ac.in>
2021-09-21 15:39:45 +0530

    [ADD] Associations
    - Each user can have more than one playerstocks, but each playerstock is related to only one user
    - Each playerstock can have only one stock, but each stock can have more than one playerstocks

--- src/models/index.js
+++ src/models/index.js
@@ -1,15 +1,41 @@
 const { Sequelize } = require('sequelize')
 
+// Create connection object
 const sequelize = new Sequelize(process.env.PG_DB_URI, {
   logging: process.env.LOGGING === 'false' ? false : console.log
 })
 
+// Define models
 const modelDefiners = [
-  require('./users')
+  require('./users'),
+  require('./playerStocks'),
+  require('./stocks')
 ]
 
 for (const modelDefiner of modelDefiners) {
   modelDefiner(sequelize)
+}
+
+// Define associations
+const { models: { User, PlayerStock, Stock } } = sequelize
+try {
+  // Each user has many playerstocks
+  User.hasMany(PlayerStock, {
+    foreignKey: {
+      allowNull: false
+    }
+  })
+  PlayerStock.belongsTo(User)
+
+  // Each playerStock is related to one stock
+  Stock.hasMany(PlayerStock, {
+    foreignKey: {
+      allowNull: false
+    }
+  })
+  PlayerStock.belongsTo(Stock)
+} catch (error) {
+  console.log('Unable to define associations:', error)
 }
 
 module.exports = sequelize
