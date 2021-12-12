275d7c9a5a8cf2e240c39f6a19aec6941ec0658f
CyberCitizen01 <mr.20u10448@btech.nitdgp.ac.in>
2021-12-12 20:56:19 +0530

    [UPDATE] transactions and assets route(s)

--- src/routes/assets.js
+++ src/routes/assets.js
@@ -3,9 +3,24 @@ const { Router } = require('express')
 const router = Router()
 
 router
-  .get('/', (req, res) => {
+  .get('/', (req, res, next) => {
     req.user.getAssets({ include: 'Stock' })
-      .then((assets) => res.status(200).json(assets))
+      .then((assets) => res.status(200).json({
+        message: `GET ${req.originalUrl} success.`,
+        assets: assets.map((asset) => ({
+          asset: {
+            quantity: asset.quantity,
+            invested: asset.invested,
+            updatedAt: asset.updatedAt
+          },
+          Stock: {
+            ...asset.Stock.toJSON(),
+            id: undefined,
+            createdAt: undefined
+          }
+        }))
+      }))
+      .catch(next)
   })
 
 module.exports = router
--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -4,17 +4,24 @@ const { models: { Transaction, Stock, Asset } } = require('../models')
 const router = Router()
 
 router.route('/')
-  .get((req, res) => {
+  .get((req, res, next) => {
     req.user.getTransactions({
       include: {
-        model: 'Stock',
+        model: Stock,
         attributes: ['name', 'code']
       }
     })
       .then(transactions => res.status(200).json({
         message: `GET ${req.originalUrl} success.`,
-        transactions
+        transactions: transactions.map((transaction) => ({
+          ...transaction.toJSON(),
+          id: undefined,
+          PlayerId: undefined,
+          StockId: undefined,
+          updatedAt: undefined
+        }))
       }))
+      .catch(next)
   })
   .post(async (req, res) => {
     if (!req.query.type || !req.body.code || !req.body.quantity) {
@@ -34,23 +41,27 @@ router.route('/')
       return
     }
 
+    const type = req.query.type
+    const code = req.body.code
+    const quantity = parseInt(req.body.quantity)
+
     const stock = await Stock.findOne({
       attributes: ['id', 'latestPrice'],
       where: {
-        code: req.body.code
+        code
       }
     })
     if (stock === null) {
       res.status(404).json({
-        message: `Stock of code ${req.body.code} not found`,
+        message: `Stock of code ${code} not found`,
         query: req.query,
         body: req.body
       })
       return
     }
     const transaction = Transaction.build({
-      type: req.query.type,
-      quantity: req.body.quantity,
+      type,
+      quantity,
       price: stock.latestPrice,
       StockId: stock.id,
       PlayerId: req.user.id
@@ -62,9 +73,9 @@ router.route('/')
       }
     })
 
-    const transactionAmount = stock.latestPrice * req.body.quantity
+    const transactionAmount = stock.latestPrice * quantity
 
-    switch (req.query.type) {
+    switch (type) {
       case 'bought': {
         if (!(req.user.valueInCash >= transactionAmount)) {
           res.status(403).json({
@@ -75,25 +86,25 @@ router.route('/')
           return
         }
         transaction.netProfit = 0
-        asset.quantity += req.body.quantity
+        asset.quantity += quantity
         asset.invested += transactionAmount
         break
       }
       case 'sold': {
-        if (isNew || req.body.quantity > asset.quantity) {
+        if (isNew || quantity > asset.quantity) {
           res.status(403).json({
             message: 'Forbidden, ' + (
               isNew
-                ? `You don't have an asset for ${req.body.code}`
+                ? `You don't have an asset for ${code}`
                 : `You have only ${asset.quantity} stocks`
             ),
             query: req.query,
             body: req.body
           })
         }
-        const costBasis = req.body.quantity * (asset.invested) / (asset.quantity)
+        const costBasis = quantity * (asset.invested) / (asset.quantity)
         transaction.netProfit = transactionAmount - costBasis
-        asset.quantity -= req.body.quantity
+        asset.quantity -= quantity
         asset.invested -= transactionAmount
         break
       }
@@ -102,8 +113,23 @@ router.route('/')
     await asset.save()
     res.status(200).json({
       message: `POST ${req.originalUrl} success.`,
-      transaction,
-      asset
+      transaction: {
+        ...transaction.toJSON(),
+        id: undefined,
+        PlayerId: undefined,
+        StockId: undefined,
+        updatedAt: undefined
+      },
+      asset: {
+        ...asset.toJSON(),
+        id: undefined,
+        PlayerId: undefined,
+        StockId: undefined,
+        createdAt: undefined
+      },
+      Stock: {
+        code
+      }
     })
   })
 
