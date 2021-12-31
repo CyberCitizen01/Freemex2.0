aa68b4fe37912d83fb0fc036938383d1f9408406
Mohammad Rafivulla <77384412+CyberCitizen01@users.noreply.github.com>
2021-12-31 22:23:54 +0530

    [ADD] Admin routes
    + Added CRUD API Routes for all models (This is true even if another model
      is added/removed), except the routes in `spareRoutes` (Also updated base path from `/admin` to `/admin/api`).
      NOTE: With this the whole security of the app depends on admin authentication.
    + TODO: When a stable express 5 realeases, update request handlers (L32-L80)
      to remove try-catch. Refer: https://expressjs.com/en/guide/error-handling.html (see the section for handling
      errors in asynchronous request handlers).

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,34 +1,104 @@
 const { Router } = require('express')
-const { models: { Player } } = require('../models')
+const { pluralize } = require('inflection')
+const { models } = require('../models')
 
 const router = Router()
 
-router.route('/')
-  .post((req, res) => {
-    Player.create(req.body, {
-      fields: ['uuid', 'username', 'name', 'email']
+const spareRoutes = [
+  /** 
+   * Add routes (RegEx) to exclude.
+   * Eg. /^DELETE \/admin\/api\/players/
+   */
+]
+
+for (const [name, model] of Object.entries(models)) {
+  router.route(`/${pluralize(name.toLowerCase())}`)
+    .use((req, res, next) => {
+      if (
+        spareRoutes.some((route) => (
+          route.test(`${req.method} ${req.originalUrl}`)
+        ))
+      ) {
+        res.sendStatus(404)
+        return
+      }
+      next()
     })
-      .then((player) => {
+    /**
+     * TODO - When a stable express 5 realeases, update request
+     * handlers to remove try-catch.
+     * Refer: https://expressjs.com/en/guide/error-handling.html
+     */
+    .get(async (req, res, next) => {
+      try {
+        const { options } = req.body
+        const instances = await model.find(options)
+        res.locals.body = instances        
+      } catch (error) {
+        next(error)
+        return
+      }
+      next()
+    })
+    .post(async (req, res, next) => {
+      try {
+        const { instance, options } = req.body
+        instance = await model.create(instance, options)
+        res.locals.body = instance        
+      } catch (error) {
+        next(error)
+        return
+      }
+      next()
+    })
+    .put(async (req, res, next) => {
+      try {
+        const { instance, options } = req.body
+        instance = await model.update(instance, options)
+        res.locals.body = instance        
+      } catch (error) {
+        next(error)
+        return
+      }
+      next()
+    })
+    .delete(async (req, res, next) => {
+      try {
+        const { options } = req.body
+        /**
+         * To prevent destroy everything, refer:
+         * https://sequelize.org/v6/manual/model-querying-basics.html#simple-delete-queries
+         */
+        options.truncate = false
+        instance = await model.delete(options)
+        res.locals.body = instance        
+      } catch (error) {
+        next(error)
+        return
+      }
+      next()
+    })
+    .use((req, res, next) => {
+      if (res.locals.body) {
         res.status(200).json({
-          message: `POST ${req.originalUrl} success.`,
-          player
+          message: `${req.method} ${req.originalUrl} success.`,
+          [name]: res.locals.body
         })
-      })
-      .catch((error) => {
-        console.log('Unable to create player instance:', error)
-        if (error.name === 'SequelizeUniqueConstraintError') {
-          res.status(403).json({
-            message: 'Unable to create player instance',
-            details: error.original.detail,
-            body: req.body
-          })
-          return
-        }
-        res.status(500).json({
-          message: 'Unable to create player instance',
+        return
+      }
+      res.sendStatus(404)
+    })
+    .use((err, req, res, next) => {
+      if (err.name === 'SequelizeUniqueConstraintError') {
+        res.status(403).json({
+          message: `Unable to create ${name} instance`,
+          details: error.original.detail,
           body: req.body
         })
-      })
-  })
+        return
+      }
+      next(err)
+    })
+}
 
 module.exports = router
