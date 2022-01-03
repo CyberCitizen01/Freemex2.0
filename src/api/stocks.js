a207e67bd1874716b9059e6143125ec02a16b8e8
CyberCitizen01 <77384412+CyberCitizen01@users.noreply.github.com>
2022-01-03 21:51:09 +0530

    [ADD] Function to check credits left
    ***************************************************
    + CHANGES THE SYNTAX FOR STOCKS_API_URI
        Remove the `stock/market/batch` part from the
        end, and don't include `/` at the end.
    ***************************************************
    + Added `filter` query param as in the API docs, to get exactly what we want.
    + Some refactoring
    + Add Sort `serilizedData` according to the order of `instances`. (TODO added in 864c24cbffe326e18f2be4d708a683ad817825a01~)
    + Updated `require.main === module` block's logic, included `checkCreditsLeft`
      and added `ADF` env var for `stocksDataFactory`

--- src/api/stocks.js
+++ src/api/stocks.js
@@ -1,13 +1,21 @@
 const axios = require('axios').default
-const { CODES } = require('../utils/fixtures')
+const { CODES, STOCKS } = require('../utils/fixtures')
 
-async function getRawStocksData (symbols = CODES /* codes is analogous to symbols */) {
+const FIELDS = Object.keys(STOCKS[0]).filter((field) => !(
+  ['name', 'code'].includes(field)
+))
+
+async function getRawStocksData (
+  symbols = CODES /* codes is analogous to symbols */,
+  filter = FIELDS /* The required attributes (columns) of `Stock` table */
+) {
   const queryParams = new URLSearchParams({
     symbols,
+    filter,
     types: 'quote',
     token: process.env.STOCKS_API_TOKEN
   })
-  const url = `${process.env.STOCKS_API_URI}?${queryParams}`
+  const url = `${process.env.STOCKS_API_URI}/stock/market/batch?${queryParams}`
   try {
     const { data, status } = await axios.get(url)
     if (!data) throw Error(`recieved empty data, status:${status}`)
@@ -21,17 +29,38 @@ async function stocksDataFactory (instances) {
   const symbols = instances.map(({ code }) => code)
   const data = await getRawStocksData(symbols)
   const serializedData = []
-  // TODO - Make sure that, `serilizedData` is according to the
-  //        order of `instances`.
-  for (const [, { quote: { symbol: code, latestPrice, change, changePercent, latestUpdate } }] of Object.entries(data)) {
+  for (const [code, {
+    quote: {
+      latestPrice, change, changePercent, latestUpdate
+    }
+  }] of Object.entries(data)) {
     serializedData.push({
       code, latestPrice, change, changePercent, latestUpdate
     })
   }
+  // Sort `serilizedData` according to the
+  // order of `instances`.
+  serializedData.sort((a, b) => (
+    symbols.indexOf(a.code) - symbols.indexOf(b.code)
+  ))
   return serializedData
 }
 
-module.exports = { stocksDataFactory, getRawStocksData }
+async function checkCreditsLeft (type = 'credits') {
+  const queryParams = new URLSearchParams({
+    token: process.env.STOCKS_API_TOKEN
+  })
+  const url = `${process.env.STOCKS_API_URI}/account/usage/${type}?${queryParams}`
+  try {
+    const { data, status } = await axios.get(url)
+    if (!data) throw Error(`recieved empty data, status:${status}`)
+    return data
+  } catch (error) {
+    console.log('Unable to fetch data from stocks api:', error)
+  }
+}
+
+module.exports = { stocksDataFactory, getRawStocksData, checkCreditsLeft }
 
 if (require.main === module) {
   /**
@@ -40,15 +69,15 @@ if (require.main === module) {
   * Can be used to check if rate limit
   * for the API has exceeded.
   */
-  require('dotenv').config()
-  const symbols = process.argv.length > 2 ? process.argv.slice(2) : ['GOOG']
-  const instances = symbols.map((symbol) => ({ code: symbol }))
-  stocksDataFactory(instances)
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
-  if (process.env.ARD === '1') { // ARD => API Raw Data
-    getRawStocksData(symbols)
-      .then(data => console.log(data))
-      .catch(error => console.log(error))
-  }
+  (async () => {
+    require('dotenv').config()
+    const symbols = process.argv.length > 2 ? process.argv.slice(2) : ['GOOG']
+    if (process.env.ADF === '1') { // ADF => API Data Factory
+      const instances = symbols.map((symbol) => ({ code: symbol }))
+      console.log(await stocksDataFactory(instances).catch(console.error))
+    } else if (process.env.ARD === '1') { // ARD => API Raw Data
+      console.log(await getRawStocksData(symbols).catch(console.error))
+    }
+    console.log(await checkCreditsLeft().catch(console.error))
+  })()
 }
