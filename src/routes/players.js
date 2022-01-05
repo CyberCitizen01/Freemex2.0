e5ef82e21b1b0620c9540e1d391a4866e68b3997
CyberCitizen01 <77384412+CyberCitizen01@users.noreply.github.com>
2022-01-05 14:36:23 +0530

    [ADD] Constraints for changing username
    + Route: `/api/players?scope=username`

--- src/routes/players.js
+++ src/routes/players.js
@@ -52,7 +52,15 @@ router.route('/')
       return
     }
 
-    req.user.username = req.body.username
+    const { username } = req.body
+    if (username.length < 5) {
+      res.status(400).json({
+        message: 'Username should be of atleast 5 characters.',
+        body: req.body
+      })
+      return
+    }
+    req.user.username = username
     req.user.save({
       fields: ['username'],
       returning: ['uuid', 'username', 'email']
@@ -65,6 +73,14 @@ router.route('/')
       })
       .catch((error) => {
         console.log('Unable to update username', error)
+        if (error.name === 'SequelizeUniqueConstraintError') {
+          res.status(500).json({
+            message: 'Username should be unique.',
+            details: [error.original.detail] || error.errors.map(({ message }) => message),
+            body: req.body
+          })
+          return
+        }
         res.status(500).json({
           message: 'Unable to update username',
           query: req.query,
