1f3ce428a17092b2e038e0323864581ec7ea83f1
CyberCitizen01 <77384412+CyberCitizen01@users.noreply.github.com>
2022-01-12 02:58:39 +0530

    [UPDATE] env var pertaining to PostgreSQL connection
    + For upcomming docker conf files.

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize')
 
 // Create connection object
-const sequelize = new Sequelize(process.env.PG_DB_URI, {
-  logging: process.env.LOGGING === 'false' ? false : console.log
+const sequelize = new Sequelize({
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  dialect: 'postgres',
+  logging: process.env.SEQUELIZE_LOGGING === 'false' ? false : console.log
 })
 
 // Define models
